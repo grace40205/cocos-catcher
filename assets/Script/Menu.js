a0fe7f20d2d41554b3593f159e757c9929b18091
FXY <1968514458@qq.com>
2018-07-02 16:15:00 +0800

    1.场景设计 2.加载对话框

--- assets/Script/Menu.js
+++ assets/Script/Menu.js
@@ -3,12 +3,28 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        DemonN:cc.Node,
+        ExercisN:cc.Node,
+        ExamN:cc.Node,
        
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+     onLoad () {
+         this.DemonN.on(cc.Node.EventType.MOUSE_MOVE, function(event){
+             console.log('演示模式');
+             let tipDemon=this.getChildByName('MenuTipD');
+             tipDemon.active=true;
+
+         },this.DemonN);
+         this.ExercisN.on(cc.Node.EventType.MOUSE_MOVE, function(event){
+            cc.log('-------ExercisN-------'); 
+         }, this.ExercisN);
+         this.ExamN.on(cc.Node.EventType.MOUSE_MOVE, function(event){
+            cc.log('-------ExamN-------'); 
+         }, this.ExamN);
+     },
 
     start () {
 
